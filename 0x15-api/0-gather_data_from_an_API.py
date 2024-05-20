f64662645af95bfc2532fa92d191d8fe7693a258
Karushi536 <karushiabigael@gmail.com>
2024-05-20 23:51:27 +0300

    ilc

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
-import requests
-import sys
-
-def get_todo_list_progress(employee_id):
-    """
-    Get the TODO list progress for a given employee ID.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        None
-    """
-    # Make API request to get employee data
-    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    employee_data = response.json()
-    employee_name = employee_data.get("name", "Unknown")
-
-    # Make API request to get todo list data
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
-    todo_list_data = response.json()
-
-    # Calculate number of done tasks
-    done_tasks = [task for task in todo_list_data if task.get("completed", False)]
-    num_done_tasks = len(done_tasks)
-    total_num_tasks = len(todo_list_data)
-
-    # Print employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_num_tasks}):")
-    for task in done_tasks:
-        print(f"\t{task.get('title', 'Unknown')}")
-
+"""
+Return information for a given employee about his/her TODO list progress
+"""
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    import requests
+    import sys
 
-    employee_id = int(sys.argv[1])
-    get_todo_list_progress(employee_id)
+    DONE_TASKS = 0
+    ALL_TASKS = 0
+
+    URL_FOR_USERS = 'https://jsonplaceholder.typicode.com/users/{0}'.\
+        format(sys.argv[1])
+    URL_FOR_TODOS = 'https://jsonplaceholder.typicode.com/todos'
+    r_for_users = requests.get(URL_FOR_USERS)
+    r_for_todos = requests.get(URL_FOR_TODOS)
+
+    name = r_for_users.json().get('name')
+    todos = r_for_todos.json()
+    for todo in todos:
+        if todo.get('userId') == int(sys.argv[1]):
+            ALL_TASKS += 1
+        if (todo.get('userId') == int(sys.argv[1]))\
+                and (todo.get('completed')):
+            DONE_TASKS += 1
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, DONE_TASKS, ALL_TASKS))
+    for todo in todos:
+        if (todo.get('userId') == int(sys.argv[1]))\
+                and (todo.get('completed')):
+            print("	 {}".format(todo.get('title')))
